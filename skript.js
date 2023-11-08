aef226f76de635a22f808282da6f1f98fc8c7888
Yuldashevrozali <Yuldashevrozali08@gmail.com>
2023-11-08 21:04:11 +0500

    user

---
+++ skript.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn');
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+btn.addEventListener('click', function () {
+    const menu = document.getElementById('menu');
+    const name1 = name.value;
+    const user = {};
+    user.id = Date.now();
+    user.text = form.value;
+})

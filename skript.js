d68069225b7de5ac1ba806a9ea10fd0571d8117c
Yuldashevrozali <Yuldashevrozali08@gmail.com>
2023-11-09 06:51:17 +0500

    value

--- skript.js
+++ skript.js
@@ -1,16 +1,24 @@
 const btn = document.getElementById('btn');
 const form = document.getElementById('form');
 const email = document.getElementById('email');
+const Age = document.getElementById('number');
 btn.addEventListener('click', function () {
-    // const menu = document.getElementById('menu'); // Agar foydalanilmasa, uni o'chirib tashlang
 
-    const data = localStorage.getItem('todo') ? JSON.parse(localStorage.getItem('todo')) : [];
+    if (form.value && email.value && Age.value) {
+        const data = localStorage.getItem('todo') ? JSON.parse(localStorage.getItem('todo')) : [];
+
+        const todo = {};
+        todo.id = Date.now();
+        todo.text = form.value;
+        todo.status = 'active';
+        data.push(todo);
+
+        form.value = '';
+
+        localStorage.setItem('todo', JSON.stringify(data));
+    } 
+    alert("Bo'sh maydon bo'lmasligi zarur");
+    let li = document.createElement("li");
     
-    const todo = {};
-    todo.id = Date.now();
-    todo.text = form.value; // form.value ni email.value ga o'zgartiring
-    todo.status = 'active';
-    data.push(todo);
 
-    localStorage.setItem('todo', JSON.stringify(data))
 })

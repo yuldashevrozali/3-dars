eafad5a20209f4f4bd51392c5a697a09c13c2d38
Yuldashevrozali <Yuldashevrozali08@gmail.com>
2023-11-09 07:25:07 +0500

    li add

--- skript.js
+++ skript.js
@@ -4,13 +4,12 @@ const email = document.getElementById('email');
 const Age = document.getElementById('number');
 
 btn.addEventListener('click', function () {
-
     if (form.value && email.value && Age.value) {
         const data = localStorage.getItem('todo') ? JSON.parse(localStorage.getItem('todo')) : [];
 
         const todo = {};
         todo.id = Date.now();
-        todo.text = form.value;
+        todo.text = (form.value)
         todo.status = 'active';
         data.push(todo);
 
@@ -19,13 +18,14 @@ btn.addEventListener('click', function () {
         email.value = '';
 
         localStorage.setItem('todo', JSON.stringify(data));
-    } else{
-        alert("Bo'sh maydon bo'lmasligi zarur");
-        let li = document.createElement("li");
-    }
 
-    const ul = document.getElementById('ul');
-    let li = document.createElement('li');
-    li.setAttribute('id', 'todo_' + todo.id);
-    ul.appendChild(li);
+        const ul = document.getElementById('ul');
+        let li = document.createElement('li');
+        li.setAttribute('id', 'todo_' + todo.id);
+        li.appendChild(document.createTextNode(todo.text)); // O'zgartirilgan qism
+        ul.appendChild(li);
+    } else {
+        alert("Bo'sh maydon bo'lmasligi zarur");
+    }
 })
+

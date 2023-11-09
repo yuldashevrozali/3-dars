967ed09da215323a495756f2797d5dad0a257e59
Yuldashevrozali <Yuldashevrozali08@gmail.com>
2023-11-09 06:42:16 +0500

    localstroge push

--- skript.js
+++ skript.js
@@ -2,9 +2,15 @@ const btn = document.getElementById('btn');
 const form = document.getElementById('form');
 const email = document.getElementById('email');
 btn.addEventListener('click', function () {
-    const menu = document.getElementById('menu');
-    const name1 = name.value;
-    const user = {};
-    user.id = Date.now();
-    user.text = form.value;
+    // const menu = document.getElementById('menu'); // Agar foydalanilmasa, uni o'chirib tashlang
+
+    const data = localStorage.getItem('todo') ? JSON.parse(localStorage.getItem('todo')) : [];
+    
+    const todo = {};
+    todo.id = Date.now();
+    todo.text = form.value; // form.value ni email.value ga o'zgartiring
+    todo.status = 'active';
+    data.push(todo);
+
+    localStorage.setItem('todo', JSON.stringify(data))
 })
